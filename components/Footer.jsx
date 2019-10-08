fa1d3d5b99840c995c9a6761b790484a82bebfc3
Carmen Tang <carmen.tang@thecontrolgroup.com>
2019-10-08 12:36:59 -0700

    comment out footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,23 +6,25 @@ const Footer = props => (
     <div className="logo-trademark">
     </div>
     {/* social */}
-    <div className="social footer-item">
-      <h4 className="remove-tablet">Social</h4>
-      <ul>
-        <li>
-          <a href="">twitter</a>
-        </li>
-        <li>
-          <a href="">fb</a>
-        </li>
-        <li>
-          <a href="">linkedin</a>
-        </li>
-        <li>
-          <a href="">instagram</a>
-        </li>
-      </ul>
-    </div> 
+    {/* 
+	    <div className="social footer-item">
+	      <h4 className="remove-tablet">Social</h4>
+	      <ul>
+	        <li>
+	          <a href="">twitter</a>
+	        </li>
+	        <li>
+	          <a href="">fb</a>
+	        </li>
+	        <li>
+	          <a href="">linkedin</a>
+	        </li>
+	        <li>
+	          <a href="">instagram</a>
+	        </li>
+	      </ul>
+	    </div> 
+	*/}
   </div>
 );
 
